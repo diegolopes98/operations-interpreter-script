da75cb6186ca4bdad0b59ffd1ff31b62cfb389d5
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2025-12-26 14:28:17 -0300

    refactor: use ts type guard to improve function checks

--- src/interpreter/interpreter.ts
+++ src/interpreter/interpreter.ts
@@ -1,4 +1,10 @@
-import { Operation, parse } from '../operation';
+import {
+  Operation,
+  parse,
+  isArithmeticalOperation,
+  isComparisonOperation,
+  isNumber,
+} from '../operation';
 
 export const interpreter = (input: string): boolean => {
   const operations = parse(input);
@@ -7,28 +13,28 @@ export const interpreter = (input: string): boolean => {
 
   if (operations.length === 1) {
     const lastOp = operations.pop()!;
-    return typeof lastOp === 'number' ? lastOp > 0 : false;
+    return isNumber(lastOp) ? lastOp > 0 : false;
   }
 
   const interprete = (ops: Operation[]): number => {
     const op = ops.pop()!;
-    switch (typeof op) {
-      case 'function':
-        const el2 = interprete(ops);
-        const el1 = interprete(ops);
-        const value = op(el1, el2);
 
-        if (typeof value === 'boolean') {
-          ops.push(value ? 1 : 0);
-        } else {
-          ops.push(value);
-        }
-
-        return interprete(ops);
-
-      default:
-        return Number(op);
+    if (isNumber(op)) {
+      return op;
     }
+
+    const el2 = interprete(ops);
+    const el1 = interprete(ops);
+
+    if (isComparisonOperation(op)) {
+      ops.push(op(el1, el2) ? 1 : 0);
+    }
+
+    if (isArithmeticalOperation(op)) {
+      ops.push(op(el1, el2));
+    }
+
+    return interprete(ops);
   };
 
   const result = interprete(operations);
--- src/operation/operation.ts
+++ src/operation/operation.ts
@@ -10,18 +10,28 @@ export const equal = (el1: number, el2: number): boolean => el1 === el2;
 
 export type Operation = number | ArithmeticalOperation | ComparisonOperation;
 
-const parseSingle = (op: string): Operation => {
-  switch (op) {
-    case 'OP_ADD':
-      return add;
-    case 'OP_SUB':
-      return sub;
-    case 'OP_EQUAL':
-      return equal;
-    default:
-      return Number(op.substring(3));
-  }
-};
+export const isArithmeticalOperation = (
+  op: Operation
+): op is ArithmeticalOperation =>
+  typeof op === 'function' && typeof op(0, 0) === 'number';
+
+export const isComparisonOperation = (
+  op: Operation
+): op is ComparisonOperation =>
+  typeof op === 'function' && typeof op(0, 0) === 'boolean';
+
+export const isNumber = (op: Operation): op is number => typeof op === 'number';
 
 export const parse = (input: string): Operation[] =>
-  input.split(' ').map(parseSingle);
+  input.split(' ').map(op => {
+    switch (op) {
+      case 'OP_ADD':
+        return add;
+      case 'OP_SUB':
+        return sub;
+      case 'OP_EQUAL':
+        return equal;
+      default:
+        return Number(op.substring(3));
+    }
+  });
